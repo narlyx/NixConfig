06c7913319b316fed6b9f1a0da9d18e9a6c545be
Narlyx <109835029+narlyx@users.noreply.github.com>
2024-03-04 08:14:29 -0700

    Darwin fixes

--- modules/home-manager/appbundles/default.nix
+++ modules/home-manager/appbundles/default.nix
@@ -1,10 +1,11 @@
 { pkgs, ... }:
 
-let
-in {
+{
+  # Shell
   programs.zsh.enable = true;
   home.file.".zshrc".source = ../../../dotfiles/zsh/zshrc;
 
+  # Cli Tools
   programs.zoxide.enable  = true;
   programs.bat.enable = true;
   programs.eza.enable = true;
@@ -12,23 +13,23 @@ in {
   programs.thefuck.enable = true;
   programs.tmux.enable = true;
 
+  # Git
   programs.git.enable = true;
   home.file.".gitconfig".source = ../../../dotfiles/git/gitconfig;
   programs.gh.enable = true;
 
+  # Editors
   programs.neovim.enable = true;
   home.file.".config/nvim" = {
     source = ../../../dotfiles/nvim;
     recursive = true;
   };
 
-
-
-
+  # Todo MOVE AWAYYY
   home.packages = [ pkgs.firefox pkgs.bitwarden pkgs.spotify pkgs.discord pkgs.github-desktop pkgs.vscode pkgs.android-studio ];
 
 
-
+  # Other Dotfiles
   home.file.".config/hypr/hyprland.conf".source = ../../../dotfiles/hypr/hyprland.conf;
   home.file.".config/waybar" = {
     source = ../../../dotfiles/waybar;
